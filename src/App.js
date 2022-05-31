051ad1c02e0141fa4c8011058c9c3f47b7da95bb
totokazu <tkn.soccerwalker@gmail.com>
2022-05-31 12:05:44 +0900

    finish starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NftUploader from './components/NftUploader/NftUploader';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NftUploader></NftUploader>
     </div>
   );
 }
+
 
 export default App;
