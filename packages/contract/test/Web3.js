eaea2155269134ea3c596dea0ab105d7c80b6074
github honganji <yujicavs@icloud.com>
2023-03-21 16:18:49 +0900

    fix contract

--- packages/contract/test/Web3.js
+++ packages/contract/test/Web3.js
@@ -9,7 +9,8 @@ describe('Web3Mint', () => {
     const [owner, addr1] = await ethers.getSigners();
 
     const nftName = 'poker';
-    const ipfsCID = 'bafkreievxssucnete4vpthh3klylkv2ctll2sk2ib24jvgozyg62zdtm2y';
+    const ipfsCID =
+      'bafkreievxssucnete4vpthh3klylkv2ctll2sk2ib24jvgozyg62zdtm2y';
 
     await mintContract.connect(owner).mintIpfsNFT(nftName, ipfsCID);
     await mintContract.connect(addr1).mintIpfsNFT(nftName, ipfsCID);
