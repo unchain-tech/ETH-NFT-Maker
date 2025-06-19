92402508047116ab1c4d11fe19cb483e1a155c11
Mizue Ando <mzeand33@gmail.com>
2025-06-19 22:51:52 +0900

    Using dotenv

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
 module.exports = {
   solidity: "0.8.18",
   networks: {
